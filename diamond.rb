108e4eb040af291ffd95b68410d8c2e76aa0df61
Anselm Helbig <git@mnemonikk.org>
2017-05-06 13:28:34 +0200

    Switch to using Canvas in Diamond

--- diamond.rb
+++ diamond.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require_relative "canvas"
 
 class TestDiamond < Minitest::Test
   def test_end_to_end
@@ -20,50 +21,39 @@ C   C
   A  
     EOS
   end
-
-  def test_letters
-    assert_equal "A".chars, Diamond.new("A").letters
-    assert_equal "ABA".chars, Diamond.new("B").letters
-    assert_equal "ABCBA".chars, Diamond.new("C").letters
-  end
-
-  def test_line_for
-    assert_equal "A\n", Diamond.new("A").line_for("A")
-    assert_equal " A \n", Diamond.new("B").line_for("A")
-    assert_equal "B B\n", Diamond.new("B").line_for("B")
-    assert_equal " B B \n", Diamond.new("C").line_for("B")
-    assert_equal "C   C\n", Diamond.new("C").line_for("C")
-  end
 end
 
 Diamond = Struct.new(:last) do
   FIRST = "A"
 
   def to_s
-    lines.join
-  end
-
-  def lines
-    letters.map(&method(:line_for))
-  end
-
-  def letters
-    (FIRST...last).to_a + (FIRST..last).to_a.reverse
-  end
-
-  def line_for(letter)
-    outer_padding = " " * (width - (letter.ord - FIRST.ord))
-    if letter == FIRST
-      outer_padding + letter + outer_padding
-    else
-      inner_padding = " " * ((letter.ord - FIRST.ord) * 2 - 1)
-      outer_padding + letter + inner_padding + letter + outer_padding
-    end + "\n"
+    draw!
+    canvas.to_s
   end
 
   private
 
+  def draw!
+    FIRST.upto(last).zip(
+      0.upto(radius),
+      radius.downto(0)
+    ).each do |letter, x, y|
+      canvas[ x + radius,  y + radius] = letter
+      canvas[-x + radius,  y + radius] = letter
+      canvas[ x + radius, -y + radius] = letter
+      canvas[-x + radius, -y + radius] = letter
+    end
+  end
+
+  def canvas
+    @canvas ||= Canvas.new(width)
+  end
+
+  def radius
+    @radius ||= last.ord - FIRST.ord
+  end
+
   def width
-    last.ord - FIRST.ord
+    radius * 2 + 1
   end
 end
